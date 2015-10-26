7a59f4607c2e0adc62e7107c4f6e892aabe33e30
Dentaku65 <dentaku65@gmail.com>
2015-10-26 07:00:04 +0100

    Added correct icon

--- channels/streaming01.py
+++ channels/streaming01.py
@@ -34,7 +34,7 @@ def mainlist(item):
                      title="[COLOR azure]Ultimi Film Inseriti[/COLOR]",
                      action="peliculas",
                      url=host,
-                     thumbnail="http://dc584.4shared.com/img/XImgcB94/s7/13feaf0b538/saquinho_de_pipoca_01"),
+                     thumbnail="http://orig03.deviantart.net/6889/f/2014/079/7/b/movies_and_popcorn_folder_icon_by_matheusgrilo-d7ay4tw.png"),
                 Item(channel=__channel__,
                      title="[COLOR azure]Film Per Categoria[/COLOR]",
                      action="categorias",
