509d3a3a3490df445cbf59cb042ca5b45c0d9a9a
Dentaku65 <dentaku65@gmail.com>
2015-10-28 19:24:25 +0100

    Added Doc search tweak

--- channels/guardarefilm.py
+++ channels/guardarefilm.py
@@ -59,16 +59,20 @@ def mainlist(item):
                      url="%s/streaming-cartoni-animati/" % host,
                      thumbnail="http://orig09.deviantart.net/df5a/f/2014/169/2/a/fist_of_the_north_star_folder_icon_by_minacsky_saya-d7mq8c8.png"),
                 Item(channel=__channel__,
+                     title="[COLOR yellow]Cerca...[/COLOR]",
+                     action="search",
+                     thumbnail="http://dc467.4shared.com/img/fEbJqOum/s7/13feaf0c8c0/Search"),
+                Item(channel=__channel__,
                      title="[COLOR azure]Serie TV[/COLOR]",
                      action="peliculas",
                      extra="serie",
                      url="%s/serie-tv-streaming/" % host,
                      thumbnail="http://xbmc-repo-ackbarr.googlecode.com/svn/trunk/dev/skin.cirrus%20extended%20v2/extras/moviegenres/New%20TV%20Shows.png"),
                 Item(channel=__channel__,
-                     title="[COLOR yellow]Cerca...[/COLOR]",
+                     title="[COLOR yellow]Cerca Serie TV...[/COLOR]",
                      action="search",
+                     extra="serie",
                      thumbnail="http://dc467.4shared.com/img/fEbJqOum/s7/13feaf0c8c0/Search")]
-
     return itemlist
 
 
@@ -108,7 +112,10 @@ def search(item, texto):
     logger.info("[guardarefilm.py] " + item.url + " search " + texto)
     item.url = host + "/?do=search&subaction=search&story=" + texto
     try:
-        return peliculas(item)
+        if item.extra == "serie":
+            return peliculas(item)
+        else:
+            return peliculas(item)
     # Se captura la excepción, para no interrumpir al buscador global si un canal falla
     except:
         import sys
